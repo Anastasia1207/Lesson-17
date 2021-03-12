4802d7fd9f0bc26061c86809d9bbf5b61a08c8b7
Anastasija <78434027+Anastasia1207@users.noreply.github.com>
2021-03-12 12:49:51 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+console.log('Task-1'); 
+
+const ageGroupe = 78;
+
+if (ageGroupe  > 0 && ageGroupe < 19 ) {
+    console.log('1 - 18 years category')
+} 
+else if (ageGroupe > 18 && ageGroupe < 31  ) {
+    console.log('19 - 30 years category')
+}
+else if (ageGroupe > 30 && ageGroupe < 41) {
+    console.log('31 - 40 years category')
+}
+if (ageGroupe > 40 && ageGroupe < 51 ) {
+    console.log('41 - 50 years category')
+}
+if (ageGroupe > 50 && ageGroupe  ) {
+    console.log('51 + years category')
+}
+
+
+console.log('Task-2'); 
+
+const Number = 12;
+
+if (Number % 2 == 0) {
+    console.log('even number');
+} else  {
+    console.log('odd number');
+}
+
+
+console.log('Task-3'); 
+
+var num6 = 37;
+
+if ( num6 % 2 === 0 && num6 % 3 === 0 ) {
+    console.log('Fizz and Bazz');
+}
+else if (num6 % 2 === 0 ) { 
+console.log('Fizz');
+} 
+else if (num6 % 3 === 0 ) { 
+    console.log('Bazz');
+    } 
+else  {
+    console.log(num6);
+} 
+
+
+
+
